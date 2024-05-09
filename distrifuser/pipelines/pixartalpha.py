72c831b17ad761531aa035f40c701851748c4988
Steaunk <wchiennan@gmail.com>
2024-05-09 17:00:45 +0800

    chore: upd pixartalpha

--- distrifuser/pipelines/pixartalpha.py
+++ distrifuser/pipelines/pixartalpha.py
@@ -4,122 +4,20 @@
 import torch
 from diffusers import PixArtAlphaPipeline
 from diffusers.models.transformers.transformer_2d import Transformer2DModel
+from diffusers.pipelines.pixart_alpha.pipeline_pixart_alpha import (
+    ASPECT_RATIO_1024_BIN,
+    ASPECT_RATIO_512_BIN,
+    ASPECT_RATIO_256_BIN,
+)
 
 # from distrifuser.models.distri_sdxl_unet_tp import DistriSDXLUNetTP
+from distrifuser.pipelines.pip.distri_pixartalpha import DistriPixArtAlphaPiP
+from distrifuser.schedulers.pip.dpmsolver_multistep import DPMSolverMultistepSchedulerPiP
 from distrifuser.models import NaivePatchDiT, DistriDiTPP, DistriDiTPiP, DistriDiTTP
 from distrifuser.utils import DistriConfig, PatchParallelismCommManager
 from distrifuser.logger import init_logger
 
 logger = init_logger(__name__)
-
-ASPECT_RATIO_1024_BIN = {
-    "0.25": [512.0, 2048.0],
-    "0.28": [512.0, 1856.0],
-    "0.32": [576.0, 1792.0],
-    "0.33": [576.0, 1728.0],
-    "0.35": [576.0, 1664.0],
-    "0.4": [640.0, 1600.0],
-    "0.42": [640.0, 1536.0],
-    "0.48": [704.0, 1472.0],
-    "0.5": [704.0, 1408.0],
-    "0.52": [704.0, 1344.0],
-    "0.57": [768.0, 1344.0],
-    "0.6": [768.0, 1280.0],
-    "0.68": [832.0, 1216.0],
-    "0.72": [832.0, 1152.0],
-    "0.78": [896.0, 1152.0],
-    "0.82": [896.0, 1088.0],
-    "0.88": [960.0, 1088.0],
-    "0.94": [960.0, 1024.0],
-    "1.0": [1024.0, 1024.0],
-    "1.07": [1024.0, 960.0],
-    "1.13": [1088.0, 960.0],
-    "1.21": [1088.0, 896.0],
-    "1.29": [1152.0, 896.0],
-    "1.38": [1152.0, 832.0],
-    "1.46": [1216.0, 832.0],
-    "1.67": [1280.0, 768.0],
-    "1.75": [1344.0, 768.0],
-    "2.0": [1408.0, 704.0],
-    "2.09": [1472.0, 704.0],
-    "2.4": [1536.0, 640.0],
-    "2.5": [1600.0, 640.0],
-    "3.0": [1728.0, 576.0],
-    "4.0": [2048.0, 512.0],
-}
-
-ASPECT_RATIO_512_BIN = {
-    "0.25": [256.0, 1024.0],
-    "0.28": [256.0, 928.0],
-    "0.32": [288.0, 896.0],
-    "0.33": [288.0, 864.0],
-    "0.35": [288.0, 832.0],
-    "0.4": [320.0, 800.0],
-    "0.42": [320.0, 768.0],
-    "0.48": [352.0, 736.0],
-    "0.5": [352.0, 704.0],
-    "0.52": [352.0, 672.0],
-    "0.57": [384.0, 672.0],
-    "0.6": [384.0, 640.0],
-    "0.68": [416.0, 608.0],
-    "0.72": [416.0, 576.0],
-    "0.78": [448.0, 576.0],
-    "0.82": [448.0, 544.0],
-    "0.88": [480.0, 544.0],
-    "0.94": [480.0, 512.0],
-    "1.0": [512.0, 512.0],
-    "1.07": [512.0, 480.0],
-    "1.13": [544.0, 480.0],
-    "1.21": [544.0, 448.0],
-    "1.29": [576.0, 448.0],
-    "1.38": [576.0, 416.0],
-    "1.46": [608.0, 416.0],
-    "1.67": [640.0, 384.0],
-    "1.75": [672.0, 384.0],
-    "2.0": [704.0, 352.0],
-    "2.09": [736.0, 352.0],
-    "2.4": [768.0, 320.0],
-    "2.5": [800.0, 320.0],
-    "3.0": [864.0, 288.0],
-    "4.0": [1024.0, 256.0],
-}
-
-ASPECT_RATIO_256_BIN = {
-    "0.25": [128.0, 512.0],
-    "0.28": [128.0, 464.0],
-    "0.32": [144.0, 448.0],
-    "0.33": [144.0, 432.0],
-    "0.35": [144.0, 416.0],
-    "0.4": [160.0, 400.0],
-    "0.42": [160.0, 384.0],
-    "0.48": [176.0, 368.0],
-    "0.5": [176.0, 352.0],
-    "0.52": [176.0, 336.0],
-    "0.57": [192.0, 336.0],
-    "0.6": [192.0, 320.0],
-    "0.68": [208.0, 304.0],
-    "0.72": [208.0, 288.0],
-    "0.78": [224.0, 288.0],
-    "0.82": [224.0, 272.0],
-    "0.88": [240.0, 272.0],
-    "0.94": [240.0, 256.0],
-    "1.0": [256.0, 256.0],
-    "1.07": [256.0, 240.0],
-    "1.13": [272.0, 240.0],
-    "1.21": [272.0, 224.0],
-    "1.29": [288.0, 224.0],
-    "1.38": [288.0, 208.0],
-    "1.46": [304.0, 208.0],
-    "1.67": [320.0, 192.0],
-    "1.75": [336.0, 192.0],
-    "2.0": [352.0, 176.0],
-    "2.09": [368.0, 176.0],
-    "2.4": [384.0, 160.0],
-    "2.5": [400.0, 160.0],
-    "3.0": [432.0, 144.0],
-    "4.0": [512.0, 128.0],
-}
-
 
 class DistriPixArtAlphaPipeline:
     def __init__(self, pipeline: PixArtAlphaPipeline, module_config: DistriConfig):
@@ -158,14 +56,27 @@ class DistriPixArtAlphaPipeline:
         else:
             raise ValueError(f"Unknown parallelism: {distri_config.parallelism}")
         
-        transformer.to(device)
-
-        pipeline = PixArtAlphaPipeline.from_pretrained(
-            pretrained_model_name_or_path,
-            torch_dtype=torch_dtype,
-            transformer=transformer,
-            **kwargs,
-        ).to(device)
+        if distri_config.parallelism == "pipeline":
+            scheduler = DPMSolverMultistepSchedulerPiP.from_pretrained(
+                pretrained_model_name_or_path, 
+                subfolder="scheduler"
+            )
+        
+        if distri_config.parallelism == "pipeline":
+            pipeline = DistriPixArtAlphaPiP.from_pretrained(
+                pretrained_model_name_or_path,
+                torch_dtype=torch_dtype,
+                transformer=transformer,
+                **kwargs,
+            ).to(device)
+            pipeline.init(distri_config)
+        else:
+            pipeline = PixArtAlphaPipeline.from_pretrained(
+                pretrained_model_name_or_path,
+                torch_dtype=torch_dtype,
+                transformer=transformer,
+                **kwargs,
+            ).to(device)
 
         return DistriPixArtAlphaPipeline(pipeline, distri_config)
 

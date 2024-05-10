5d86cd138757ce5f19ac95376f30bdbad589543b
Steaunk <wchiennan@gmail.com>
2024-05-10 21:29:21 +0800

    feat: add pixartalpha pipeline pre run

--- distrifuser/pipelines/pixartalpha.py
+++ distrifuser/pipelines/pixartalpha.py
@@ -15,7 +15,7 @@ from distrifuser.pipelines.pip.distri_pixartalpha import DistriPixArtAlphaPiP
 from distrifuser.schedulers.pip.dpmsolver_multistep import DPMSolverMultistepSchedulerPiP
 from diffusers import DPMSolverMultistepScheduler
 from distrifuser.models import NaivePatchDiT, DistriDiTPP, DistriDiTPiP, DistriDiTTP
-from distrifuser.utils import DistriConfig, PatchParallelismCommManager
+from distrifuser.utils import DistriConfig, PatchParallelismCommManager, PipelineParallelismCommManager
 from distrifuser.logger import init_logger
 
 logger = init_logger(__name__)
@@ -121,130 +121,142 @@ class DistriPixArtAlphaPipeline:
 
         batch_size = distri_config.batch_size or 1
         num_images_per_prompt = 1
-
-        # Resolution binning
-        if distri_config.use_resolution_binning:
-            if pipeline.transformer.config.sample_size == 128:
-                aspect_ratio_bin = ASPECT_RATIO_1024_BIN
-            elif pipeline.transformer.config.sample_size == 64:
-                aspect_ratio_bin = ASPECT_RATIO_512_BIN
-            elif pipeline.transformer.config.sample_size == 32:
-                aspect_ratio_bin = ASPECT_RATIO_256_BIN
-            else:
-                raise ValueError("Invalid sample size")
-            orig_height, orig_width = height, width
-            height, width = pipeline.height, width = pipeline.classify_height_width_bin(
-                height, width, ratios=aspect_ratio_bin
+        
+        if distri_config.parallelism == "pipeline":
+            self.pipeline(
+                height=distri_config.height,
+                width=distri_config.width,
+                prompt="",
+                use_resolution_binning=distri_config.use_resolution_binning,
+                num_inference_steps=distri_config.warmup_steps + 2
             )
+            comm_manager = PipelineParallelismCommManager(distri_config)    
+            self.pipeline.set_comm_manager(comm_manager)
 
-        # Encode input prompt
-        (
-            prompt_embeds,
-            prompt_attention_mask,
-            negative_prompt_embeds,
-            negative_prompt_attention_mask,
-        ) = self.pipeline.encode_prompt(
-            prompt="",
-            do_classifier_free_guidance=distri_config.do_classifier_free_guidance,
-            device=device,
-        )
+        else:
+            # Resolution binning
+            if distri_config.use_resolution_binning:
+                if pipeline.transformer.config.sample_size == 128:
+                    aspect_ratio_bin = ASPECT_RATIO_1024_BIN
+                elif pipeline.transformer.config.sample_size == 64:
+                    aspect_ratio_bin = ASPECT_RATIO_512_BIN
+                elif pipeline.transformer.config.sample_size == 32:
+                    aspect_ratio_bin = ASPECT_RATIO_256_BIN
+                else:
+                    raise ValueError("Invalid sample size")
+                orig_height, orig_width = height, width
+                height, width = pipeline.height, width = pipeline.classify_height_width_bin(
+                    height, width, ratios=aspect_ratio_bin
+                )
 
-        if distri_config.do_classifier_free_guidance:
-            prompt_embeds = torch.cat([negative_prompt_embeds, prompt_embeds], dim=0)
-            prompt_attention_mask = torch.cat(
-                [negative_prompt_attention_mask, prompt_attention_mask], dim=0
+            # Encode input prompt
+            (
+                prompt_embeds,
+                prompt_attention_mask,
+                negative_prompt_embeds,
+                negative_prompt_attention_mask,
+            ) = self.pipeline.encode_prompt(
+                prompt="",
+                do_classifier_free_guidance=distri_config.do_classifier_free_guidance,
+                device=device,
             )
-
-        # Prepare added time ids & embeddings
-
-        t = torch.zeros([2], device=device, dtype=torch.long)
-
-        guidance_scale = 4.0
-        latent_size = pipeline.transformer.config.sample_size
-        # latents = torch.zeros(
-        #     [batch_size, latent_channels, latent_size, latent_size],
-        #     device=device,
-        #     dtype=pipeline.transformer.dtype,
-        # )
-
-        latent_channels = pipeline.transformer.config.in_channels
-        latents = pipeline.prepare_latents(
-            batch_size,
-            latent_channels,
-            height,
-            width,
-            prompt_embeds.dtype,
-            device,
-            None,
-        )
-        latent_model_input = (
-            torch.cat([latents, latents], 0) if guidance_scale > 1 else latents
-        )
-
-        # encoder_hidden_states.shape torch.Size([2, 120, 4096])
-        # encoder_attention_mask.shape torch.Size([2, 120])
-        # resolution.shape torch.Size([2, 2])
-        # aspect_ratio.shape torch.Size([2, 1])
-        static_inputs["hidden_states"] = latent_model_input
-        static_inputs["timestep"] = t
-        static_inputs["encoder_hidden_states"] = prompt_embeds
-        static_inputs["encoder_attention_mask"] = prompt_attention_mask
-        added_cond_kwargs = {"resolution": None, "aspect_ratio": None}
-        if pipeline.transformer.config.sample_size == 128:
-            resolution = torch.tensor([0, 0]).repeat(
-                batch_size * num_images_per_prompt, 1
-            )
-            aspect_ratio = torch.tensor([0.0]).repeat(
-                batch_size * num_images_per_prompt, 1
-            )
-            resolution = resolution.to(dtype=prompt_embeds.dtype, device=device)
-            aspect_ratio = aspect_ratio.to(dtype=prompt_embeds.dtype, device=device)
 
             if distri_config.do_classifier_free_guidance:
-                resolution = torch.cat([resolution, resolution], dim=0)
-                aspect_ratio = torch.cat([aspect_ratio, aspect_ratio], dim=0)
+                prompt_embeds = torch.cat([negative_prompt_embeds, prompt_embeds], dim=0)
+                prompt_attention_mask = torch.cat(
+                    [negative_prompt_attention_mask, prompt_attention_mask], dim=0
+                )
 
-            added_cond_kwargs = {"resolution": resolution, "aspect_ratio": aspect_ratio}
-        static_inputs["added_cond_kwargs"] = added_cond_kwargs
+            # Prepare added time ids & embeddings
 
-        # Used to create communication buffer
-        comm_manager = None
-        if distri_config.n_device_per_batch > 1:
-            comm_manager = PatchParallelismCommManager(distri_config)
-            pipeline.transformer.set_comm_manager(comm_manager)
+            t = torch.zeros([2], device=device, dtype=torch.long)
 
-            # Only used for creating the communication buffer
+            guidance_scale = 4.0
+            latent_size = pipeline.transformer.config.sample_size
+            # latents = torch.zeros(
+            #     [batch_size, latent_channels, latent_size, latent_size],
+            #     device=device,
+            #     dtype=pipeline.transformer.dtype,
+            # )
+
+            latent_channels = pipeline.transformer.config.in_channels
+            latents = pipeline.prepare_latents(
+                batch_size,
+                latent_channels,
+                height,
+                width,
+                prompt_embeds.dtype,
+                device,
+                None,
+            )
+            latent_model_input = (
+                torch.cat([latents, latents], 0) if guidance_scale > 1 else latents
+            )
+
+            # encoder_hidden_states.shape torch.Size([2, 120, 4096])
+            # encoder_attention_mask.shape torch.Size([2, 120])
+            # resolution.shape torch.Size([2, 2])
+            # aspect_ratio.shape torch.Size([2, 1])
+            static_inputs["hidden_states"] = latent_model_input
+            static_inputs["timestep"] = t
+            static_inputs["encoder_hidden_states"] = prompt_embeds
+            static_inputs["encoder_attention_mask"] = prompt_attention_mask
+            added_cond_kwargs = {"resolution": None, "aspect_ratio": None}
+            if pipeline.transformer.config.sample_size == 128:
+                resolution = torch.tensor([0, 0]).repeat(
+                    batch_size * num_images_per_prompt, 1
+                )
+                aspect_ratio = torch.tensor([0.0]).repeat(
+                    batch_size * num_images_per_prompt, 1
+                )
+                resolution = resolution.to(dtype=prompt_embeds.dtype, device=device)
+                aspect_ratio = aspect_ratio.to(dtype=prompt_embeds.dtype, device=device)
+
+                if distri_config.do_classifier_free_guidance:
+                    resolution = torch.cat([resolution, resolution], dim=0)
+                    aspect_ratio = torch.cat([aspect_ratio, aspect_ratio], dim=0)
+
+                added_cond_kwargs = {"resolution": resolution, "aspect_ratio": aspect_ratio}
+            static_inputs["added_cond_kwargs"] = added_cond_kwargs
+
+            # Used to create communication buffer
+            comm_manager = None
+            if distri_config.n_device_per_batch > 1:
+                comm_manager = PatchParallelismCommManager(distri_config)
+                pipeline.transformer.set_comm_manager(comm_manager)
+
+                # Only used for creating the communication buffer
+                pipeline.transformer.set_counter(0)
+                pipeline.transformer(**static_inputs, return_dict=False, record=True)
+                if comm_manager.numel > 0:
+                    comm_manager.create_buffer()
+
+            # Pre-run
             pipeline.transformer.set_counter(0)
             pipeline.transformer(**static_inputs, return_dict=False, record=True)
-            if comm_manager.numel > 0:
-                comm_manager.create_buffer()
 
-        # Pre-run
-        pipeline.transformer.set_counter(0)
-        pipeline.transformer(**static_inputs, return_dict=False, record=True)
+            if distri_config.use_cuda_graph:
+                if comm_manager is not None:
+                    comm_manager.clear()
+                if distri_config.parallelism == "naive_patch":
+                    counters = [0, 1]
+                elif distri_config.parallelism == "patch":
+                    counters = [
+                        0,
+                        distri_config.warmup_steps + 1,
+                        distri_config.warmup_steps + 2,
+                    ]
+                else:
+                    raise ValueError(f"Unknown parallelism: {distri_config.parallelism}")
+                for counter in counters:
+                    graph = torch.cuda.CUDAGraph()
+                    with torch.cuda.graph(graph):
+                        pipeline.transformer.set_counter(counter)
+                        output = pipeline.transformer(
+                            **static_inputs, return_dict=False, record=True
+                        )[0]
+                        static_outputs.append(output)
+                    cuda_graphs.append(graph)
+                pipeline.transformer.setup_cuda_graph(static_outputs, cuda_graphs)
 
-        if distri_config.use_cuda_graph:
-            if comm_manager is not None:
-                comm_manager.clear()
-            if distri_config.parallelism == "naive_patch":
-                counters = [0, 1]
-            elif distri_config.parallelism == "patch":
-                counters = [
-                    0,
-                    distri_config.warmup_steps + 1,
-                    distri_config.warmup_steps + 2,
-                ]
-            else:
-                raise ValueError(f"Unknown parallelism: {distri_config.parallelism}")
-            for counter in counters:
-                graph = torch.cuda.CUDAGraph()
-                with torch.cuda.graph(graph):
-                    pipeline.transformer.set_counter(counter)
-                    output = pipeline.transformer(
-                        **static_inputs, return_dict=False, record=True
-                    )[0]
-                    static_outputs.append(output)
-                cuda_graphs.append(graph)
-            pipeline.transformer.setup_cuda_graph(static_outputs, cuda_graphs)
-
-        self.static_inputs = static_inputs
+            self.static_inputs = static_inputs

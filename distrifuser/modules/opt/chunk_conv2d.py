053e9fac8f448fd73946563c3a8afb1b9671d6bf
pengcheng yang <yangpc615@gmail.com>
2024-05-22 21:25:55 +0800

    fix padding bug of chunkconv2d and add unit test

--- distrifuser/modules/opt/chunk_conv2d.py
+++ distrifuser/modules/opt/chunk_conv2d.py
@@ -5,6 +5,9 @@ from torch import Tensor
 from torch.nn import functional as F
 from torch.nn.common_types import _size_2_t
 
+from distrifuser.logger import init_logger
+
+logger = init_logger(__name__)
 
 class ChunkConv2d(nn.Conv2d):
     __doc__ = r"""
@@ -35,12 +38,21 @@ class ChunkConv2d(nn.Conv2d):
             inner_input = F.pad(input,
                                 self._reversed_padding_repeated_twice,
                                 mode=self.padding_mode)
-        _, _, h, w = input.shape
+        _, _, h, w = inner_input.shape
         if self.chunk_size <= 0 or (h <= self.chunk_size
                                     and w <= self.chunk_size):
             return F.conv2d(inner_input, weight, bias, self.stride,
                             self.padding, self.dilation, self.groups)
 
+        if self.padding != 0:
+            if isinstance(self.padding, int):
+                padding_tuple = (self.padding,) * 4
+            elif isinstance(self.padding, tuple):
+                padding_tuple = (self.padding[0],) * 2 + (self.padding[1],) * 2
+            else:
+                raise ValueError(f"padding should be int or tuple, type:{type(self.padding)}")
+            inner_input = F.pad(inner_input, padding_tuple, mode="constant")
+        _, _, h, w = inner_input.shape
         num_chunks_in_h = (h + self.chunk_size - 1) // self.chunk_size
         num_chunks_in_w = (w + self.chunk_size - 1) // self.chunk_size
         unit_chunk_size_h = h // num_chunks_in_h
@@ -65,7 +77,7 @@ class ChunkConv2d(nn.Conv2d):
         def correct_end(end, kernel_size, stride):
             return ((end + stride - 1) // stride - 1) * stride + kernel_size
 
-        def correct_start(start, kernel_size, stride):
+        def correct_start(start, stride):
             return ((start + stride - 1) // stride) * stride
 
         outputs = []
@@ -86,13 +98,13 @@ class ChunkConv2d(nn.Conv2d):
                     end_h = h
 
                 if idx_w > 0:
-                    start_w = correct_start(start_w, kernel_size_w, stride_w)
+                    start_w = correct_start(start_w, stride_w)
                 if idx_h > 0:
-                    start_h = correct_start(start_h, kernel_size_h, stride_h)
+                    start_h = correct_start(start_h, stride_h)
 
                 inner_output.append(
                     F.conv2d(inner_input[:, :, start_h:end_h, start_w:end_w],
-                             weight, bias, self.stride, self.padding,
+                             weight, bias, self.stride, 0,
                              self.dilation, self.groups))
             outputs.append(torch.cat(inner_output, dim=-1))
         return torch.cat(outputs, dim=-2)
@@ -103,28 +115,30 @@ class PatchConv2d:
     def __init__(self, chunk_size: int = -1):
         self.chunk_size = chunk_size
 
-    def __cell__(self, module: nn.Module) -> nn.Module:
+    def __call__(self, module: nn.Module) -> nn.Module:
 
         def replace_conv2d(module, name):
             if not isinstance(module, nn.Module):
                 raise ValueError(
                     f"module should be nn.Module, type: {type(module)}")
-            for attr_str in dir(module):
-                target_attr = getattr(module, attr_str)
-                if isinstance(target_attr, nn.Conv2d):
+
+            for name, child_module in module.named_children():
+                if isinstance(child_module, nn.Conv2d):
                     chunk_conv2d = ChunkConv2d(
-                        target_attr.in_channels, target_attr.out_channels,
-                        target_attr.kernel_size, target_attr.stride,
-                        target_attr.padding, target_attr.dilation,
-                        target_attr.groups, target_attr.bias,
-                        target_attr.padding_mode, target_attr.device,
-                        target_attr.dtype, self.chunk_size)
-                    chunk_conv2d.weight.data = target_attr.weight.data
-                    chunk_conv2d.bias.data = target_attr.bias.data
-                    setattr(module, attr_str, chunk_conv2d)
+                        child_module.in_channels, child_module.out_channels,
+                        child_module.kernel_size, child_module.stride,
+                        child_module.padding, child_module.dilation,
+                        child_module.groups, child_module.bias is not None,
+                        child_module.padding_mode, child_module.weight.device,
+                        child_module.weight.dtype, self.chunk_size)
+                    chunk_conv2d.weight.data = child_module.weight.data
+                    chunk_conv2d.bias.data = child_module.bias.data
+                    setattr(module, name, chunk_conv2d)
+                    logger.info(f"{name} is replaced by ChunkConv2d")
+                replace_conv2d(child_module, name)
 
-            for name, immediate_child_module in module.named_children():
-                replace_conv2d(immediate_child_module, name)
-
-        replace_conv2d(module, 'model')
+        for attr_str in dir(module):
+            target_attr = getattr(module, attr_str)
+            if isinstance(target_attr, nn.Module):
+                replace_conv2d(target_attr, attr_str)
         return module

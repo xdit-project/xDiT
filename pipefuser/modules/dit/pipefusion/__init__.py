8a3c5e296d42e50fc4fcf00a044c04f0c98669f0
Steaunk <wchiennan@gmail.com>
2024-06-20 18:06:45 +0000

    feat: add distri SDTransformer2DModel

--- pipefuser/modules/dit/pipefusion/__init__.py
+++ pipefuser/modules/dit/pipefusion/__init__.py
@@ -1,4 +1,4 @@
-from .transformer2d import DistriTransformer2DModel
+from .transformer_2d import DistriTransformer2DModel
 from .attn import DistriCrossAttentionPiP, DistriSelfAttentionPiP
 from .conv2d import DistriConv2dPiP
 from .patchembed import DistriPatchEmbed

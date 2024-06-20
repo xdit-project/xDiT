9fb92ff5a95b74e81af0dfb384f478e21dd53690
Steaunk <wchiennan@gmail.com>
2024-06-20 17:52:18 +0000

    fix: upd patch_embed to v0.29.0

--- pipefuser/modules/dit/pipefusion/patchembed.py
+++ pipefuser/modules/dit/pipefusion/patchembed.py
@@ -1,4 +1,4 @@
-# adapted from https://github.com/huggingface/diffusers/blob/main/src/diffusers/models/embeddings.py
+# adapted from https://github.com/huggingface/diffusers/blob/v0.29.0/src/diffusers/models/embeddings.py
 import torch
 
 from diffusers.models.embeddings import PatchEmbed, get_2d_sincos_pos_embed
@@ -15,6 +15,7 @@ class DistriPatchEmbed(BaseModule):
         self.batch_idx = 0
         self.pos_embed = None
 
+    
     def forward(self, latent):
         module = self.module
         distri_config = self.distri_config
@@ -23,16 +24,27 @@ class DistriPatchEmbed(BaseModule):
             or self.counter <= distri_config.warmup_steps
         )
 
+        logger.info(f"{module.pos_embed_max_size}")
+
         if is_warmup:
-            height, width = (
-                latent.shape[-2] // module.patch_size,
-                latent.shape[-1] // module.patch_size,
-            )
+            if module.pos_embed_max_size is not None:
+                height, width = latent.shape[-2:]
+            else:
+                height, width = (
+                    latent.shape[-2] // module.patch_size,
+                    latent.shape[-1] // module.patch_size,
+                )
         else:
-            height, width = (
-                latent.shape[-2] // module.patch_size * distri_config.pp_num_patch,
-                latent.shape[-1] // module.patch_size,
-            )
+            if module.pos_embed_max_size is not None:
+                height, width = (
+                    latent.shape[-2] * distri_config.pp_num_patch,
+                    latent.shape[-1],
+                )
+            else:
+                height, width = (
+                    latent.shape[-2] // module.patch_size * distri_config.pp_num_patch,
+                    latent.shape[-1] // module.patch_size,
+                )
 
         latent = module.proj(latent)
         if module.flatten:
@@ -47,20 +59,23 @@ class DistriPatchEmbed(BaseModule):
         # (For PixArt-Alpha: https://github.com/PixArt-alpha/PixArt-alpha/blob/0f55e922376d8b797edd44d25d0e7464b260dcab/diffusion/model/nets/PixArtMS.py#L162C151-L162C160)
 
         # TODO: There might be a more faster way to generate a smaller pos_embed
-        if module.height != height or module.width != width:
-            pos_embed = get_2d_sincos_pos_embed(
-                embed_dim=module.pos_embed.shape[-1],
-                grid_size=(height, width),
-                base_size=module.base_size,
-                interpolation_scale=module.interpolation_scale,
-            )
-            pos_embed = torch.from_numpy(pos_embed)
-            module.pos_embed = pos_embed.float().unsqueeze(0).to(latent.device)
-            module.height = height
-            module.width = width
-            pos_embed = module.pos_embed
+        if module.pos_embed_max_size:
+            pos_embed = module.cropped_pos_embed(height, width)
         else:
-            pos_embed = module.pos_embed
+            if module.height != height or module.width != width:
+                pos_embed = get_2d_sincos_pos_embed(
+                    embed_dim=module.pos_embed.shape[-1],
+                    grid_size=(height, width),
+                    base_size=module.base_size,
+                    interpolation_scale=module.interpolation_scale,
+                )
+                pos_embed = torch.from_numpy(pos_embed)
+                module.pos_embed = pos_embed.float().unsqueeze(0).to(latent.device)
+                module.height = height
+                module.width = width
+                pos_embed = module.pos_embed
+            else:
+                pos_embed = module.pos_embed
         b, c, h = pos_embed.shape
 
         if not is_warmup:
@@ -76,4 +91,5 @@ class DistriPatchEmbed(BaseModule):
                 self.batch_idx = 0
                 self.counter += 1
 
+        logger.info(f"{latent.shape} {pos_embed.shape}")
         return (latent + pos_embed).to(latent.dtype)

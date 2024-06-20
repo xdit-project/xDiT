56352932d277a9bea53092f00c027e3022c06bdb
feifeibear <fangjiarui123@gmail.com>
2024-06-20 09:55:26 +0000

    add missing python package

--- setup.py
+++ setup.py
@@ -14,14 +14,19 @@ if __name__ == "__main__":
         author_email="muyangli@mit.edu",
         packages=find_packages(),
         install_requires=[
-            "torch>=2.2", 
-            "diffusers==0.27.2", 
-            "transformers", 
-            "tqdm", 
+            "torch>=2.2",
+            "diffusers==0.27.2",
+            "transformers",
+            "tqdm",
+            "sentencepiece",
+            "accelerate",
             f"patchvae @ file://localhost/{os.path.join(os.getcwd(), 'pipefuser/modules/patchvae')}#egg=patchvae",
         ],
-        dependency_links = [
-            "file://" + os.path.join(os.getcwd(), "pipefuser/modules/patchvae#egg=patchvae-0.0.0b1")
+        dependency_links=[
+            "file://"
+            + os.path.join(
+                os.getcwd(), "pipefuser/modules/patchvae#egg=patchvae-0.0.0b1"
+            )
         ],
         url="https://github.com/PipeFusion/PipeFusion.",
         description="DistriFusion: Distributed Parallel Inference for High-Resolution Diffusion Models",

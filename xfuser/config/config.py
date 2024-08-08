0bcf3411237eb126152df793b93c8d338e7e80ac
Jinzhe Pan <48981407+Eigensystem@users.noreply.github.com>
2024-08-08 16:11:16 +0800

    [Feat] add running scripts for all models (#173)

--- xfuser/config/config.py
+++ xfuser/config/config.py
@@ -56,18 +56,13 @@ class RuntimeConfig:
 @dataclass
 class DataParallelConfig():
     dp_degree: int = 1
-    use_split_batch: bool = False
-    do_classifier_free_guidance: bool = True
+    use_cfg_parallel: bool = False
 
     def __post_init__(self):
         assert self.dp_degree >= 1, "dp_degree must greater than or equal to 1"
 
-        if self.use_split_batch and not self.do_classifier_free_guidance:
-            logger.warning(f"Split batch is enabled, "
-                           f"set classifier_free_guidance to True")
-            self.do_classifier_free_guidance = True
         # set classifier_free_guidance_degree parallel for split batch
-        if self.use_split_batch and self.do_classifier_free_guidance:
+        if self.use_cfg_parallel:
             self.cfg_degree = 2
         else:
             self.cfg_degree = 1

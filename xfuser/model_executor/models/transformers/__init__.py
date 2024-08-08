be9ac6cece0bbc5cdf9d13e4cfd05ececbcbc4c8
Jinzhe Pan <48981407+Eigensystem@users.noreply.github.com>
2024-08-08 09:54:43 +0800

    [Feat] supported Flux (#168)

--- xfuser/model_executor/models/transformers/__init__.py
+++ xfuser/model_executor/models/transformers/__init__.py
@@ -2,10 +2,12 @@ from .register import xFuserTransformerWrappersRegister
 from .base_transformer import xFuserTransformerBaseWrapper
 from .pixart_transformer_2d import xFuserPixArtTransformer2DWrapper
 from .transformer_sd3 import xFuserSD3Transformer2DWrapper
+from .transformer_flux import xFuserFluxTransformer2DWrapper
 
 __all__ = [
     "xFuserTransformerWrappersRegister",
     "xFuserTransformerBaseWrapper",
     "xFuserPixArtTransformer2DWrapper",
     "xFuserSD3Transformer2DWrapper",
+    "xFuserFluxTransformer2DWrapper",
 ]

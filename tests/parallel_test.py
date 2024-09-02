25b9d1c633e042aa9530f13ac93faf3800986d36
Jinzhe Pan <48981407+Eigensystem@users.noreply.github.com>
2024-09-02 11:29:48 +0800

    feat: support interface for huggingface (#245)

---
+++ tests/parallel_test.py
@@ -0,0 +1,81 @@
+from xfuser.parallel import xdit_parallel
+
+import time
+import os
+import torch
+import torch.distributed as dist
+from diffusers import StableDiffusion3Pipeline
+
+from xfuser import xFuserArgs
+from xfuser.config import FlexibleArgumentParser
+from xfuser.core.distributed import (
+    get_world_group,
+    is_dp_last_group,
+    get_data_parallel_world_size,
+    get_runtime_state,
+)
+
+
+def main():
+    parser = FlexibleArgumentParser(description="xFuser Arguments")
+    args = xFuserArgs.add_cli_args(parser).parse_args()
+    engine_args = xFuserArgs.from_cli_args(args)
+    engine_config, input_config = engine_args.create_config()
+
+    pipe = StableDiffusion3Pipeline.from_pretrained(
+        pretrained_model_name_or_path=engine_config.model_config.model,
+        torch_dtype=torch.float16,
+    )
+
+    pipe = xdit_parallel(pipe, engine_config)
+    local_rank = get_world_group().local_rank
+    pipe.to(f"cuda:{local_rank}")
+
+    pipe.prepare_run(input_config)
+
+    torch.cuda.reset_peak_memory_stats()
+    start_time = time.time()
+    output = pipe(
+        height=input_config.height,
+        width=input_config.height,
+        prompt=input_config.prompt,
+        num_inference_steps=input_config.num_inference_steps,
+        output_type=input_config.output_type,
+        generator=torch.Generator(device="cuda").manual_seed(input_config.seed),
+    )
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    peak_memory = torch.cuda.max_memory_allocated(device=f"cuda:{local_rank}")
+
+    parallel_info = (
+        f"dp{engine_args.data_parallel_degree}_cfg{engine_config.parallel_config.cfg_degree}_"
+        f"ulysses{engine_args.ulysses_degree}_ring{engine_args.ring_degree}_"
+        f"pp{engine_args.pipefusion_parallel_degree}_patch{engine_args.num_pipeline_patch}"
+    )
+    if input_config.output_type == "pil":
+        global_rank = get_world_group().rank
+        dp_group_world_size = get_data_parallel_world_size()
+        dp_group_index = global_rank // dp_group_world_size
+        num_dp_groups = engine_config.parallel_config.dp_degree
+        dp_batch_size = (input_config.batch_size + num_dp_groups - 1) // num_dp_groups
+        if is_dp_last_group():
+            if not os.path.exists("results"):
+                os.mkdir("results")
+            for i, image in enumerate(output.images):
+                image_rank = dp_group_index * dp_batch_size + i
+                image.save(
+                    f"./results/stable_diffusion_3_result_{parallel_info}_{image_rank}.png"
+                )
+                print(
+                    f"image {i} saved to ./results/stable_diffusion_3_result_{parallel_info}_{image_rank}.png"
+                )
+
+    if get_world_group().rank == get_world_group().world_size - 1:
+        print(
+            f"{parallel_info} epoch time: {elapsed_time:.2f} sec, memory: {peak_memory/1e9} GB"
+        )
+    get_runtime_state().destory_distributed_env()
+
+
+if __name__ == "__main__":
+    main()

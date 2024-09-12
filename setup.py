7f174dc1a6efa67356dabd9ab15ece3996bb3745
C <ichengzeyi@gmail.com>
2024-09-12 17:47:56 +0800

    CogVideoX support with USP (#261)

--- setup.py
+++ setup.py
@@ -12,40 +12,34 @@ def get_cuda_version():
     except Exception as e:
         return 'no_cuda'
 
-def get_install_requires(cuda_version):
-    if cuda_version == 'cu124':
-        sys.stderr.write("WARNING: Manual installation required for CUDA 12.4 specific PyTorch version.\n")
-        sys.stderr.write("Please install PyTorch for CUDA 12.4 using the following command:\n")
-        sys.stderr.write("pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124\n")
-
-    return [
-        "torch==2.3.0",
-        "diffusers>=0.30.0",
-        "transformers>=4.39.1",
-        "sentencepiece>=0.1.99",
-        "accelerate==0.33.0",
-        "beautifulsoup4>=4.12.3",
-        "distvae",
-        "yunchang==0.3",
-        "flash_attn>=2.6.3",
-        "pytest",
-        "flask",
-    ]
-
 if __name__ == "__main__":
     with open("README.md", "r") as f:
         long_description = f.read()
     fp = open("xfuser/__version__.py", "r").read()
     version = eval(fp.strip().split()[-1])
 
-    cuda_version = get_cuda_version()
-
     setup(
         name="xfuser",
         author="xDiT Team",
         author_email="fangjiarui123@gmail.com",
         packages=find_packages(),
-        install_requires=get_install_requires(cuda_version),
+        install_requires=[
+            "torch>=2.3.0",
+            "accelerate==0.33.0",
+            "diffusers>=0.30.0",
+            "transformers>=4.39.1",
+            "sentencepiece>=0.1.99",
+            "beautifulsoup4>=4.12.3",
+            "distvae",
+            "yunchang==0.3",
+            "pytest",
+            "flask",
+        ],
+        extras_require={
+            "all": [
+                "flash_attn>=2.6.3",
+            ],
+        },
         url="https://github.com/xdit-project/xDiT.",
         description="xDiT: A Scalable Inference Engine for Diffusion Transformers (DiTs) on multi-GPU Clusters",
         long_description=long_description,

8bd1298a987b65e6c11e384eb76a979727344e2a
Jinzhe Pan <eigensystem1318@gmail.com>
2024-11-05 14:41:50 +0800

    refactor: unified interface for long ctx attn

--- xfuser/core/long_ctx_attention/hybrid/attn_layer.py
+++ xfuser/core/long_ctx_attention/hybrid/attn_layer.py
@@ -1,4 +1,3 @@
-from typing import Any
 import torch
 from torch import Tensor
 
@@ -86,83 +85,6 @@ class xFuserLongContextAttention(LongContextAttention):
         Returns:
             * output (Tensor): context output
         """
-        assert causal == False, "causal attention is not applied in DiTs"
-        # 3 X (bs, seq_len/N, head_cnt, head_size) -> 3 X (bs, seq_len, head_cnt/N, head_size)
-        # scatter 2, gather 1
-        if self.use_pack_qkv:
-            # (3*bs, seq_len/N, head_cnt, head_size)
-            qkv = torch.cat([query, key, value]).continous()
-            # (3*bs, seq_len, head_cnt/N, head_size)
-            qkv = SeqAllToAll4D.apply(
-                self.ulysses_pg, qkv, self.scatter_idx, self.gather_idx
-            )
-            qkv = torch.chunk(qkv, 3, dim=0)
-            query_layer, key_layer, value_layer = qkv
-
-        else:
-            query_layer = SeqAllToAll4D.apply(
-                self.ulysses_pg, query, self.scatter_idx, self.gather_idx
-            )
-            key_layer = SeqAllToAll4D.apply(
-                self.ulysses_pg, key, self.scatter_idx, self.gather_idx
-            )
-            value_layer = SeqAllToAll4D.apply(
-                self.ulysses_pg, value, self.scatter_idx, self.gather_idx
-            )
-
-        out = self.ring_attn_fn(
-            query_layer,
-            key_layer,
-            value_layer,
-            dropout_p=dropout_p,
-            softmax_scale=softmax_scale,
-            causal=causal,
-            window_size=window_size,
-            alibi_slopes=alibi_slopes,
-            deterministic=deterministic,
-            return_attn_probs=return_attn_probs,
-            group=self.ring_pg,
-            attn_layer=attn if self.use_kv_cache else None,
-        )
-
-        if type(out) == tuple:
-            context_layer, _, _ = out
-        else:
-            context_layer = out
-
-        # (bs, seq_len, head_cnt/N, head_size) -> (bs, seq_len/N, head_cnt, head_size)
-        # scatter 1, gather 2
-        output = SeqAllToAll4D.apply(
-            self.ulysses_pg, context_layer, self.gather_idx, self.scatter_idx
-        )
-
-        # out e.g., [s/p::h]
-        return output
-
-
-class xFuserJointLongContextAttention(xFuserLongContextAttention):
-    @torch.compiler.disable
-    def forward(
-        self,
-        attn,
-        query: Tensor,
-        key: Tensor,
-        value: Tensor,
-        *,
-        joint_tensor_query,
-        joint_tensor_key,
-        joint_tensor_value,
-        dropout_p=0.0,
-        softmax_scale=None,
-        causal=False,
-        window_size=(-1, -1),
-        alibi_slopes=None,
-        deterministic=False,
-        return_attn_probs=False,
-        joint_strategy="rear",
-    ):
-        # 3 X (bs, seq_len/N, head_cnt, head_size) -> 3 X (bs, seq_len, head_cnt/N, head_size)
-        # scatter 2, gather 1
         supported_joint_strategy = ["none", "front", "rear"]
         if joint_strategy not in supported_joint_strategy:
             raise ValueError(
@@ -178,133 +100,30 @@ class xFuserJointLongContextAttention(xFuserLongContextAttention):
             query = torch.cat([joint_tensor_query, query], dim=1)
         else:
             pass
-            
-        ulysses_world_size = torch.distributed.get_world_size(self.ulysses_pg)
-        ulysses_rank = torch.distributed.get_rank(self.ulysses_pg)
-        attn_heads_per_ulysses_rank = joint_tensor_key.shape[-2] // ulysses_world_size
-        joint_tensor_key = joint_tensor_key[
-            ...,
-            attn_heads_per_ulysses_rank
-            * ulysses_rank : attn_heads_per_ulysses_rank
-            * (ulysses_rank + 1),
-            :,
-        ]
-        joint_tensor_value = joint_tensor_value[
-            ...,
-            attn_heads_per_ulysses_rank
-            * ulysses_rank : attn_heads_per_ulysses_rank
-            * (ulysses_rank + 1),
-            :,
-        ]
 
-        if self.use_pack_qkv:
-            # (3*bs, seq_len/N, head_cnt, head_size)
-            qkv = torch.cat([query, key, value]).continous()
-            # (3*bs, seq_len, head_cnt/N, head_size)
-            qkv = SeqAllToAll4D.apply(
-                self.ulysses_pg, qkv, self.scatter_idx, self.gather_idx
+        if joint_strategy != "none":
+            ulysses_world_size = torch.distributed.get_world_size(self.ulysses_pg)
+            ulysses_rank = torch.distributed.get_rank(self.ulysses_pg)
+            attn_heads_per_ulysses_rank = (
+                joint_tensor_key.shape[-2] // ulysses_world_size
             )
-            qkv = torch.chunk(qkv, 3, dim=0)
-            query_layer, key_layer, value_layer = qkv
-
-        else:
-            query_layer = SeqAllToAll4D.apply(
-                self.ulysses_pg, query, self.scatter_idx, self.gather_idx
-            )
-            key_layer = SeqAllToAll4D.apply(
-                self.ulysses_pg, key, self.scatter_idx, self.gather_idx
-            )
-            value_layer = SeqAllToAll4D.apply(
-                self.ulysses_pg, value, self.scatter_idx, self.gather_idx
-            )
-
-        out = self.ring_attn_fn(
-            query_layer,
-            key_layer,
-            value_layer,
-            dropout_p=dropout_p,
-            softmax_scale=softmax_scale,
-            causal=causal,
-            window_size=window_size,
-            alibi_slopes=alibi_slopes,
-            deterministic=deterministic,
-            return_attn_probs=return_attn_probs,
-            group=self.ring_pg,
-            attn_layer=attn if self.use_kv_cache else None,
-            joint_tensor_key=joint_tensor_key,
-            joint_tensor_value=joint_tensor_value,
-            joint_strategy=joint_strategy,
-        )
-
-        if type(out) == tuple:
-            context_layer, _, _ = out
-        else:
-            context_layer = out
-
-        # (bs, seq_len, head_cnt/N, head_size) -> (bs, seq_len/N, head_cnt, head_size)
-        # scatter 1, gather 2
-        output = SeqAllToAll4D.apply(
-            self.ulysses_pg, context_layer, self.gather_idx, self.scatter_idx
-        )
-
-        # out e.g., [s/p::h]
-        return output
-
-
-class xFuserFluxLongContextAttention(xFuserLongContextAttention):
-    @torch.compiler.disable
-    def forward(
-        self,
-        attn,
-        query: Tensor,
-        key: Tensor,
-        value: Tensor,
-        *,
-        joint_tensor_query,
-        joint_tensor_key,
-        joint_tensor_value,
-        dropout_p=0.0,
-        softmax_scale=None,
-        causal=False,
-        window_size=(-1, -1),
-        alibi_slopes=None,
-        deterministic=False,
-        return_attn_probs=False,
-        joint_strategy="front",
-    ) -> Tensor:
-        """forward
-
-        Arguments:
-            query (Tensor): query input to the layer
-            key (Tensor): key input to the layer
-            value (Tensor): value input to the layer
-            args: other args
-
-        Returns:
-            * output (Tensor): context output
-        """
+            joint_tensor_key = joint_tensor_key[
+                ...,
+                attn_heads_per_ulysses_rank
+                * ulysses_rank : attn_heads_per_ulysses_rank
+                * (ulysses_rank + 1),
+                :,
+            ]
+            joint_tensor_value = joint_tensor_value[
+                ...,
+                attn_heads_per_ulysses_rank
+                * ulysses_rank : attn_heads_per_ulysses_rank
+                * (ulysses_rank + 1),
+                :,
+            ]
 
         # 3 X (bs, seq_len/N, head_cnt, head_size) -> 3 X (bs, seq_len, head_cnt/N, head_size)
         # scatter 2, gather 1
-        query = torch.cat([joint_tensor_query, query], dim=1)
-        ulysses_world_size = torch.distributed.get_world_size(self.ulysses_pg)
-        ulysses_rank = torch.distributed.get_rank(self.ulysses_pg)
-        attn_heads_per_ulysses_rank = joint_tensor_key.shape[-2] // ulysses_world_size
-        joint_tensor_key = joint_tensor_key[
-            ...,
-            attn_heads_per_ulysses_rank
-            * ulysses_rank : attn_heads_per_ulysses_rank
-            * (ulysses_rank + 1),
-            :,
-        ]
-        joint_tensor_value = joint_tensor_value[
-            ...,
-            attn_heads_per_ulysses_rank
-            * ulysses_rank : attn_heads_per_ulysses_rank
-            * (ulysses_rank + 1),
-            :,
-        ]
-
         if self.use_pack_qkv:
             # (3*bs, seq_len/N, head_cnt, head_size)
             qkv = torch.cat([query, key, value]).continous()

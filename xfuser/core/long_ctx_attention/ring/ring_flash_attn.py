1c31746e2f903e791bc2a41a0bc23614958e46cd
Jiarui Fang <fangjiarui123@gmail.com>
2024-11-05 17:58:32 +0800

    add unitest for xfuser attn layer (#335)

--- xfuser/core/long_ctx_attention/ring/ring_flash_attn.py
+++ xfuser/core/long_ctx_attention/ring/ring_flash_attn.py
@@ -1,5 +1,6 @@
 import torch
 from flash_attn.flash_attn_interface import _flash_attn_forward
+from xfuser.core.long_ctx_attention import xFuserLongContextAttention
 from xfuser.core.cache_manager.cache_manager import get_cache_manager
 from yunchang.ring.utils import RingComm, update_out_and_lse
 from yunchang.ring.ring_flash_attn import RingFlashAttnFunc

8f4b9d30ccf278aef1e7ae985f59f7c186371d41
Jiarui Fang（方佳瑞） <fangjiarui123@gmail.com>
2025-03-20 10:00:29 +0800

    bump version to 0.4.3 (#483)

--- xfuser/__init__.py
+++ xfuser/__init__.py
@@ -21,6 +21,7 @@ __all__ = [
     "xFuserHunyuanDiTPipeline",
     "xFuserCogVideoXPipeline",
     "xFuserConsisIDPipeline",
+    "xFuserStableDiffusionXLPipeline",
     "xFuserArgs",
     "EngineConfig",
     "xDiTParallel",

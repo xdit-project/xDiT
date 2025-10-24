a7b2aeba3e2fff046265d628a030de1bc56fb25e
Aleksi Vesanto <avesanto@amd.com>
2025-10-24 11:19:57 +0000

    Update base_model to wrap all attention wrappers, not just the global one

--- xfuser/model_executor/layers/__init__.py
+++ xfuser/model_executor/layers/__init__.py
@@ -1,6 +1,7 @@
 from .register import xFuserLayerWrappersRegister
 from .base_layer import xFuserLayerBaseWrapper
 from .attention_processor import xFuserAttentionWrapper
+from .attention_processor import xFuserAttentionBaseWrapper
 from .conv import xFuserConv2dWrapper
 from .embeddings import xFuserPatchEmbedWrapper
 from .feedforward import xFuserFeedForwardWrapper
@@ -8,6 +9,7 @@ from .feedforward import xFuserFeedForwardWrapper
 __all__ = [
     "xFuserLayerWrappersRegister",
     "xFuserLayerBaseWrapper",
+    "xFuserAttentionBaseWrapper",
     "xFuserAttentionWrapper",
     "xFuserConv2dWrapper",
     "xFuserPatchEmbedWrapper",

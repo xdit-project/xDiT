adb210055e027917b4b27e976b4a9a5e5a2b1d77
Aleksi Vesanto <avesanto@amd.com>
2025-10-30 10:28:31 +0000

    Set compile mode to default

--- examples/wan_example.py
+++ examples/wan_example.py
@@ -231,9 +231,9 @@ def main():
 
     if engine_config.runtime_config.use_torch_compile:
         torch._inductor.config.reorder_for_compute_comm_overlap = True
-        pipe.transformer = torch.compile(pipe.transformer, mode="max-autotune-no-cudagraphs")
+        pipe.transformer = torch.compile(pipe.transformer, mode="default")
         if pipe.transformer_2 is not None:
-            pipe.transformer_2 = torch.compile(pipe.transformer_2, mode="max-autotune-no-cudagraphs")
+            pipe.transformer_2 = torch.compile(pipe.transformer_2, mode="default")
 
         # one step to warmup the torch compiler
         _ = run_pipe(input_config, image)

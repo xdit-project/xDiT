a6bd6db5ac041d05428d645f21b29a5e4a3c00c9
Kristian Sikiric <ksikiric@chi-mi300x-017.ord.vultr.cpe.ice.amd.com>
2025-11-18 11:51:19 +0000

    Added guarding so that AITER call does not break for earlier AITER commits that does not support changing the round mode

--- xfuser/model_executor/layers/usp.py
+++ xfuser/model_executor/layers/usp.py
@@ -30,8 +30,11 @@ if HAS_FLASH_ATTN:
 HAS_AITER = env_info["has_aiter"]
 if HAS_AITER:
     import aiter
-    import os
-    HOW_V3_BF16_CVT = int(os.environ.get("HOW_V3_BF16_CVT", "2"))
+    import inspect
+    HAS_ROUND_MODE = inspect.signature(aiter.ops.mha.flash_attn_func).parameters.get("how_v3_bf16_cvt") is not None
+    if HAS_ROUND_MODE:
+        import os
+        HOW_V3_BF16_CVT = int(os.environ.get("HOW_V3_BF16_CVT", "2"))
 
 aten = torch.ops.aten
 
@@ -177,16 +180,27 @@ def _aiter_attn_call(query, key, value, dropout_p, is_causal):
     query = torch.permute(query, [0, 2, 1, 3]).contiguous()
     key = torch.permute(key, [0, 2, 1, 3]).contiguous()
     value = torch.permute(value, [0, 2, 1, 3]).contiguous()
-    output, softmax_lse = aiter.flash_attn_func(
-        query,
-        key,
-        value,
-        dropout_p=dropout_p,
-        causal=is_causal,
-        return_attn_probs=False,
-        return_lse=True,
-        how_v3_bf16_cvt=HOW_V3_BF16_CVT
-    )
+    if HAS_ROUND_MODE:
+        output, softmax_lse = aiter.ops.mha.flash_attn_func(
+            query,
+            key,
+            value,
+            dropout_p=dropout_p,
+            causal=is_causal,
+            return_attn_probs=False,
+            return_lse=True,
+            how_v3_bf16_cvt=HOW_V3_BF16_CVT
+        )
+    else:
+        output, softmax_lse = aiter.flash_attn_func(
+            query,
+            key,
+            value,
+            dropout_p=dropout_p,
+            causal=is_causal,
+            return_attn_probs=False,
+            return_lse=True
+        )
     output = torch.permute(output, [0, 2, 1, 3])
     return output, softmax_lse
 

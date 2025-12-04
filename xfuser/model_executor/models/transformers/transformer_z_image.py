dcf017f1c3adc185c2fa6a289ac76d55a95903c3
Aleksi Vesanto <avesanto@amd.com>
2025-12-04 12:52:45 +0000

    Integrate new diffusers changes to transformer

--- xfuser/model_executor/models/transformers/transformer_z_image.py
+++ xfuser/model_executor/models/transformers/transformer_z_image.py
@@ -5,6 +5,8 @@ from typing import List, Optional
 
 from diffusers.models.transformers.transformer_z_image import ZImageTransformer2DModel
 from diffusers.models.attention_processor import Attention
+from diffusers.models.modeling_outputs import Transformer2DModelOutput
+
 
 from xfuser.model_executor.layers.usp import USP
 
@@ -106,26 +108,20 @@ class xFuserZImageTransformer2DWrapper(ZImageTransformer2DModel):
 
 
     def _chunk_and_pad_sequence(self, x: torch.Tensor, sp_world_rank: int, sp_world_size: int, pad_amount: int, dim: int) -> torch.Tensor:
-        is_list = False
-        if type(x) is list:
-            x = x[0]
-            is_list = True
         if pad_amount > 0:
             if dim < 0:
                 dim = x.ndim + dim
             pad_shape = list(x.shape)
             pad_shape[dim] = pad_amount
             x = torch.cat([x,
-                           torch.zeros(
-                               pad_shape,
-                               dtype=x.dtype,
-                               device=x.device,
-                           )], dim=dim)
+                        torch.zeros(
+                            pad_shape,
+                            dtype=x.dtype,
+                            device=x.device,
+                        )], dim=dim)
         x = torch.chunk(x,
                         sp_world_size,
                         dim=dim)[sp_world_rank]
-        if is_list:
-            return [x]
         return x
 
     def _gather_and_unpad(self, x: torch.Tensor, pad_amount: int, dim: int) -> torch.Tensor:
@@ -134,143 +130,154 @@ class xFuserZImageTransformer2DWrapper(ZImageTransformer2DModel):
         return x.narrow(dim=dim, start=0, length=size - pad_amount)
 
     def forward(
-            self,
-            x: List[torch.Tensor],
-            t,
-            cap_feats: List[torch.Tensor],
-            patch_size=2,
-            f_patch_size=1,
-        ):
-            assert patch_size in self.all_patch_size
-            assert f_patch_size in self.all_f_patch_size
-            sp_world_rank = get_sequence_parallel_rank()
-            sp_world_size = get_sequence_parallel_world_size()
+        self,
+        x: List[torch.Tensor],
+        t,
+        cap_feats: List[torch.Tensor],
+        patch_size=2,
+        f_patch_size=1,
+        return_dict: bool = True,
+    ):
+        assert patch_size in self.all_patch_size
+        assert f_patch_size in self.all_f_patch_size
+        sp_world_rank = get_sequence_parallel_rank()
+        sp_world_size = get_sequence_parallel_world_size()
 
 
-            bsz = len(x)
-            device = x[0].device
-            t = t * self.t_scale
-            t = self.t_embedder(t)
+        bsz = len(x)
+        device = x[0].device
+        t = t * self.t_scale
+        t = self.t_embedder(t)
 
-            (
-                x,
-                cap_feats,
-                x_size,
-                x_pos_ids,
-                cap_pos_ids,
-                x_inner_pad_mask,
-                cap_inner_pad_mask,
-            ) = self.patchify_and_embed(x, cap_feats, patch_size, f_patch_size)
+        (
+            x,
+            cap_feats,
+            x_size,
+            x_pos_ids,
+            cap_pos_ids,
+            x_inner_pad_mask,
+            cap_inner_pad_mask,
+        ) = self.patchify_and_embed(x, cap_feats, patch_size, f_patch_size)
 
-            # x embed & refine
-            x_item_seqlens = [len(_) for _ in x]
-            assert all(_ % SEQ_MULTI_OF == 0 for _ in x_item_seqlens)
-            x_max_item_seqlen = max(x_item_seqlens)
+        # x embed & refine
+        x_item_seqlens = [len(_) for _ in x]
+        assert all(_ % SEQ_MULTI_OF == 0 for _ in x_item_seqlens)
+        x_max_item_seqlen = max(x_item_seqlens)
 
-            x = torch.cat(x, dim=0)
-            x = self.all_x_embedder[f"{patch_size}-{f_patch_size}"](x)
+        x = torch.cat(x, dim=0)
+        x = self.all_x_embedder[f"{patch_size}-{f_patch_size}"](x)
 
-            # Match t_embedder output dtype to x for layerwise casting compatibility
-            adaln_input = t.type_as(x)
-            x[torch.cat(x_inner_pad_mask)] = self.x_pad_token
-            x = list(x.split(x_item_seqlens, dim=0))
-            x_freqs_cis = list(self.rope_embedder(torch.cat(x_pos_ids, dim=0)).split(x_item_seqlens, dim=0))
+        # Match t_embedder output dtype to x for layerwise casting compatibility
+        adaln_input = t.type_as(x)
+        x[torch.cat(x_inner_pad_mask)] = self.x_pad_token
+        x = list(x.split(x_item_seqlens, dim=0))
+        x_freqs_cis = list(self.rope_embedder(torch.cat(x_pos_ids, dim=0)).split([len(_) for _ in x_pos_ids], dim=0))
 
-            x = pad_sequence(x, batch_first=True, padding_value=0.0)
-            x_freqs_cis = pad_sequence(x_freqs_cis, batch_first=True, padding_value=0.0)
-            x_attn_mask = torch.zeros((bsz, x_max_item_seqlen), dtype=torch.bool, device=device)
-            for i, seq_len in enumerate(x_item_seqlens):
-                x_attn_mask[i, :seq_len] = 1
+        x = pad_sequence(x, batch_first=True, padding_value=0.0)
+        x_freqs_cis = pad_sequence(x_freqs_cis, batch_first=True, padding_value=0.0)
+        # Clarify the length matches to satisfy Dynamo due to "Symbolic Shape Inference" to avoid compilation errors
+        x_freqs_cis = x_freqs_cis[:, : x.shape[1]]
 
-            # # SP support
-            pad_amount = (sp_world_size - (x.shape[1] % sp_world_size)) % sp_world_size
-            x = self._chunk_and_pad_sequence(x, sp_world_rank, sp_world_size, pad_amount, dim=-2)
-            x_attn_mask = self._chunk_and_pad_sequence(x_attn_mask, sp_world_rank, sp_world_size, pad_amount, dim=-1)
-            x_freqs_cis_chunked = self._chunk_and_pad_sequence(x_freqs_cis, sp_world_rank, sp_world_size, pad_amount, dim=-2)
+        x_attn_mask = torch.zeros((bsz, x_max_item_seqlen), dtype=torch.bool, device=device)
+        for i, seq_len in enumerate(x_item_seqlens):
+            x_attn_mask[i, :seq_len] = 1
 
-            if torch.is_grad_enabled() and self.gradient_checkpointing:
-                for layer in self.noise_refiner:
-                    x = self._gradient_checkpointing_func(layer, x, x_attn_mask, x_freqs_cis_chunked, adaln_input)
-            else:
-                for layer in self.noise_refiner:
-                    x = layer(x, x_attn_mask, x_freqs_cis_chunked, adaln_input)
+        # # SP support
+        pad_amount = (sp_world_size - (x.shape[1] % sp_world_size)) % sp_world_size
+        x = self._chunk_and_pad_sequence(x, sp_world_rank, sp_world_size, pad_amount, dim=-2)
+        x_attn_mask = self._chunk_and_pad_sequence(x_attn_mask, sp_world_rank, sp_world_size, pad_amount, dim=-1)
+        x_freqs_cis_chunked = self._chunk_and_pad_sequence(x_freqs_cis, sp_world_rank, sp_world_size, pad_amount, dim=-2)
 
-            # Gather SP outputs and remove padding
-            x = self._gather_and_unpad(x, pad_amount, dim=-2)
+        if torch.is_grad_enabled() and self.gradient_checkpointing:
+            for layer in self.noise_refiner:
+                x = self._gradient_checkpointing_func(layer, x, x_attn_mask, x_freqs_cis_chunked, adaln_input)
+        else:
+            for layer in self.noise_refiner:
+                x = layer(x, x_attn_mask, x_freqs_cis_chunked, adaln_input)
 
-            # cap embed & refine
-            cap_item_seqlens = [len(_) for _ in cap_feats]
-            assert all(_ % SEQ_MULTI_OF == 0 for _ in cap_item_seqlens)
-            cap_max_item_seqlen = max(cap_item_seqlens)
+        # Gather SP outputs and remove padding
+        x = self._gather_and_unpad(x, pad_amount, dim=-2)
 
-            cap_feats = torch.cat(cap_feats, dim=0)
-            cap_feats = self.cap_embedder(cap_feats)
-            cap_feats[torch.cat(cap_inner_pad_mask)] = self.cap_pad_token
-            cap_feats = list(cap_feats.split(cap_item_seqlens, dim=0))
-            cap_freqs_cis = list(self.rope_embedder(torch.cat(cap_pos_ids, dim=0)).split(cap_item_seqlens, dim=0))
+        # cap embed & refine
+        cap_item_seqlens = [len(_) for _ in cap_feats]
+        assert all(_ % SEQ_MULTI_OF == 0 for _ in cap_item_seqlens)
+        cap_max_item_seqlen = max(cap_item_seqlens)
 
-            cap_feats = pad_sequence(cap_feats, batch_first=True, padding_value=0.0)
-            cap_freqs_cis = pad_sequence(cap_freqs_cis, batch_first=True, padding_value=0.0)
-            cap_attn_mask = torch.zeros((bsz, cap_max_item_seqlen), dtype=torch.bool, device=device)
-            for i, seq_len in enumerate(cap_item_seqlens):
-                cap_attn_mask[i, :seq_len] = 1
+        cap_feats = torch.cat(cap_feats, dim=0)
+        cap_feats = self.cap_embedder(cap_feats)
+        cap_feats[torch.cat(cap_inner_pad_mask)] = self.cap_pad_token
+        cap_feats = list(cap_feats.split(cap_item_seqlens, dim=0))
+        cap_freqs_cis = list(self.rope_embedder(torch.cat(cap_pos_ids, dim=0)).split([len(_) for _ in cap_pos_ids], dim=0))
 
+        cap_feats = pad_sequence(cap_feats, batch_first=True, padding_value=0.0)
+        cap_freqs_cis = pad_sequence(cap_freqs_cis, batch_first=True, padding_value=0.0)
+        # Clarify the length matches to satisfy Dynamo due to "Symbolic Shape Inference" to avoid compilation errors
+        cap_freqs_cis = cap_freqs_cis[:, : cap_feats.shape[1]]
 
-            # # SP support
-            pad_amount = (sp_world_size - (cap_feats.shape[1] % sp_world_size)) % sp_world_size
-            cap_feats = self._chunk_and_pad_sequence(cap_feats, sp_world_rank, sp_world_size, pad_amount, dim=-2)
-            cap_attn_mask = self._chunk_and_pad_sequence(cap_attn_mask, sp_world_rank, sp_world_size, pad_amount, dim=-1)
-            cap_freqs_cis_chunked = self._chunk_and_pad_sequence(cap_freqs_cis, sp_world_rank, sp_world_size, pad_amount, dim=-2)
-
-            if torch.is_grad_enabled() and self.gradient_checkpointing:
-                for layer in self.context_refiner:
-                    cap_feats = self._gradient_checkpointing_func(layer, cap_feats, cap_attn_mask, cap_freqs_cis_chunked)
-            else:
-                for layer in self.context_refiner:
-                    cap_feats = layer(cap_feats, cap_attn_mask, cap_freqs_cis_chunked)
-
-            # Gather SP outputs and remove padding
-            cap_feats = self._gather_and_unpad(cap_feats, pad_amount, dim=-2)
+        cap_attn_mask = torch.zeros((bsz, cap_max_item_seqlen), dtype=torch.bool, device=device)
+        for i, seq_len in enumerate(cap_item_seqlens):
+            cap_attn_mask[i, :seq_len] = 1
 
 
-            # unified
-            unified = []
-            unified_freqs_cis = []
-            for i in range(bsz):
-                x_len = x_item_seqlens[i]
-                cap_len = cap_item_seqlens[i]
-                unified.append(torch.cat([x[i][:x_len], cap_feats[i][:cap_len]]))
-                unified_freqs_cis.append(torch.cat([x_freqs_cis[i][:x_len], cap_freqs_cis[i][:cap_len]]))
-            unified_item_seqlens = [a + b for a, b in zip(cap_item_seqlens, x_item_seqlens)]
-            assert unified_item_seqlens == [len(_) for _ in unified]
-            unified_max_item_seqlen = max(unified_item_seqlens)
+        # # SP support
+        pad_amount = (sp_world_size - (cap_feats.shape[1] % sp_world_size)) % sp_world_size
+        cap_feats = self._chunk_and_pad_sequence(cap_feats, sp_world_rank, sp_world_size, pad_amount, dim=-2)
+        cap_attn_mask = self._chunk_and_pad_sequence(cap_attn_mask, sp_world_rank, sp_world_size, pad_amount, dim=-1)
+        cap_freqs_cis_chunked = self._chunk_and_pad_sequence(cap_freqs_cis, sp_world_rank, sp_world_size, pad_amount, dim=-2)
 
-            unified = pad_sequence(unified, batch_first=True, padding_value=0.0)
-            unified_freqs_cis = pad_sequence(unified_freqs_cis, batch_first=True, padding_value=0.0)
-            unified_attn_mask = torch.zeros((bsz, unified_max_item_seqlen), dtype=torch.bool, device=device)
-            for i, seq_len in enumerate(unified_item_seqlens):
-                unified_attn_mask[i, :seq_len] = 1
+        if torch.is_grad_enabled() and self.gradient_checkpointing:
+            for layer in self.context_refiner:
+                cap_feats = self._gradient_checkpointing_func(layer, cap_feats, cap_attn_mask, cap_freqs_cis_chunked)
+        else:
+            for layer in self.context_refiner:
+                cap_feats = layer(cap_feats, cap_attn_mask, cap_freqs_cis_chunked)
 
-            # SP support
-            pad_amount = (sp_world_size - (unified.shape[1] % sp_world_size)) % sp_world_size
-            unified = self._chunk_and_pad_sequence(unified, sp_world_rank, sp_world_size, pad_amount, dim=-2)
-            unified_attn_mask = self._chunk_and_pad_sequence(unified_attn_mask, sp_world_rank, sp_world_size, pad_amount, dim=-1)
-            unified_freqs_cis = self._chunk_and_pad_sequence(unified_freqs_cis, sp_world_rank, sp_world_size, pad_amount, dim=-2)
+        # Gather SP outputs and remove padding
+        cap_feats = self._gather_and_unpad(cap_feats, pad_amount, dim=-2)
 
-            if torch.is_grad_enabled() and self.gradient_checkpointing:
-                for layer in self.layers:
-                    unified = self._gradient_checkpointing_func(
-                        layer, unified, unified_attn_mask, unified_freqs_cis, adaln_input
-                    )
-            else:
-                for layer in self.layers:
-                    unified = layer(unified, unified_attn_mask, unified_freqs_cis, adaln_input)
 
-            # Gather SP outputs and remove padding
-            unified = self._gather_and_unpad(unified, pad_amount, dim=-2)
+        # unified
+        unified = []
+        unified_freqs_cis = []
+        for i in range(bsz):
+            x_len = x_item_seqlens[i]
+            cap_len = cap_item_seqlens[i]
+            unified.append(torch.cat([x[i][:x_len], cap_feats[i][:cap_len]]))
+            unified_freqs_cis.append(torch.cat([x_freqs_cis[i][:x_len], cap_freqs_cis[i][:cap_len]]))
+        unified_item_seqlens = [a + b for a, b in zip(cap_item_seqlens, x_item_seqlens)]
+        assert unified_item_seqlens == [len(_) for _ in unified]
+        unified_max_item_seqlen = max(unified_item_seqlens)
 
-            unified = self.all_final_layer[f"{patch_size}-{f_patch_size}"](unified, adaln_input)
-            unified = list(unified.unbind(dim=0))
-            x = self.unpatchify(unified, x_size, patch_size, f_patch_size)
+        unified = pad_sequence(unified, batch_first=True, padding_value=0.0)
+        unified_freqs_cis = pad_sequence(unified_freqs_cis, batch_first=True, padding_value=0.0)
+        unified_attn_mask = torch.zeros((bsz, unified_max_item_seqlen), dtype=torch.bool, device=device)
+        for i, seq_len in enumerate(unified_item_seqlens):
+            unified_attn_mask[i, :seq_len] = 1
 
-            return x, {}
+        # SP support
+        pad_amount = (sp_world_size - (unified.shape[1] % sp_world_size)) % sp_world_size
+        unified = self._chunk_and_pad_sequence(unified, sp_world_rank, sp_world_size, pad_amount, dim=-2)
+        unified_attn_mask = self._chunk_and_pad_sequence(unified_attn_mask, sp_world_rank, sp_world_size, pad_amount, dim=-1)
+        unified_freqs_cis = self._chunk_and_pad_sequence(unified_freqs_cis, sp_world_rank, sp_world_size, pad_amount, dim=-2)
+
+        if torch.is_grad_enabled() and self.gradient_checkpointing:
+            for layer in self.layers:
+                unified = self._gradient_checkpointing_func(
+                    layer, unified, unified_attn_mask, unified_freqs_cis, adaln_input
+                )
+        else:
+            for layer in self.layers:
+                unified = layer(unified, unified_attn_mask, unified_freqs_cis, adaln_input)
+
+        # Gather SP outputs and remove padding
+        unified = self._gather_and_unpad(unified, pad_amount, dim=-2)
+
+        unified = self.all_final_layer[f"{patch_size}-{f_patch_size}"](unified, adaln_input)
+        unified = list(unified.unbind(dim=0))
+        x = self.unpatchify(unified, x_size, patch_size, f_patch_size)
+
+        if not return_dict:
+            return (x,)
+
+        return Transformer2DModelOutput(sample=x)
+

b9e22fda9e5f6ec94e09ac11fe18c25dd22fbc45
Aleksi Vesanto <avesanto@amd.com>
2026-01-13 10:04:33 -0600

    Add Wan T2V

--- xfuser/model_executor/runner.py
+++ xfuser/model_executor/runner.py
@@ -415,7 +415,9 @@ class xFuserZImageTurboModel(xFuserModel):
 from diffusers import WanImageToVideoPipeline, WanPipeline
 from xfuser.model_executor.models.transformers.transformer_wan import xFuserWanTransformer3DWrapper
 @register_model("Wan-AI/Wan2.2-I2V-A14B-Diffusers")
+@register_model("Wan-AI/Wan2.1-I2V-14B-720P-Diffusers")
 @register_model("Wan2.2-I2V")
+@register_model("Wan2.1-I2V")
 class xFuserWanI2VModel(xFuserModel):
 
     mod_value = 8 # vae_scale_factor_spatial * patch_size[1] = 8
@@ -497,6 +499,71 @@ class xFuserWanI2VModel(xFuserModel):
         images = input_args.get("input_images", [])
         if len(images) != 1:
             raise ValueError("Exactly one input image is required for Wan I2V model.")
+
+
+@register_model("Wan-AI/Wan2.2-T2V-A14B-Diffusers")
+@register_model("Wan-AI/Wan2.1-T2V-14B-Diffusers")
+@register_model("Wan2.2-T2V")
+@register_model("Wan2.1-T2V")
+class xFuserWanI2VModel(xFuserModel):
+
+    mod_value = 8 # vae_scale_factor_spatial * patch_size[1] = 8
+    fps = 16
+
+
+    def __init__(self, config: dict):
+        super().__init__(config)
+        self.is_wan_2_2 = "2.2" in config.model
+        if self.is_wan_2_2:
+            self.model_name: str = "Wan-AI/Wan2.2-T2V-A14B-Diffusers"
+            self.output_name: str = "wan2.2_t2v"
+        else:
+            self.model_name: str = "Wan-AI/Wan2.1-T2V-14B-Diffusers"
+            self.output_name = "wan2.1_t2v"
+        self.model_output_type: str = "video"
+
+    def _load_model(self):
+        transformer = xFuserWanTransformer3DWrapper.from_pretrained(
+            pretrained_model_name_or_path=self.model_name,
+            torch_dtype=torch.bfloat16,
+            subfolder="transformer",
+        )
+
+        if self.is_wan_2_2:
+            transformer_2 = xFuserWanTransformer3DWrapper.from_pretrained(
+                pretrained_model_name_or_path=self.model_name,
+                torch_dtype=torch.bfloat16,
+                subfolder="transformer_2",
+            )
+            pipe = WanPipeline.from_pretrained(
+                pretrained_model_name_or_path=self.model_name,
+                torch_dtype=torch.bfloat16,
+                transformer=transformer,
+                transformer_2=transformer_2,
+            )
+        else:
+            pipe = WanPipeline.from_pretrained(
+                pretrained_model_name_or_path=self.model_name,
+                torch_dtype=torch.bfloat16,
+                transformer=transformer,
+                transformer_2=transformer_2,
+            )
+
+        local_rank = get_world_group().local_rank
+        pipe = pipe.to(f"cuda:{local_rank}")
+        return pipe
+
+    def _run_pipe(self, input_args: dict):
+        output = self.pipe(
+            height=input_args["height"],
+            width=input_args["width"],
+            prompt=str(input_args["prompt"]),
+            num_inference_steps=input_args["num_inference_steps"],
+            num_frames=input_args["num_frames"],
+            guidance_scale=input_args["guidance_scale"],
+            generator=torch.Generator(device="cuda").manual_seed(input_args["seed"]),
+        )
+        return output
 
 
 

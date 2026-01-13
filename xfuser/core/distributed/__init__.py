59dd055caf35b3d7e3794ffcaf5c714b752bb2b0
Niko Sakkinen <niko.sakkinen@amd.com>
2026-01-12 22:08:06 -0800

    Added shard_model to module

--- xfuser/core/distributed/__init__.py
+++ xfuser/core/distributed/__init__.py
@@ -36,6 +36,7 @@ from .runtime_state import (
     runtime_state_is_initialized,
     initialize_runtime_state,
 )
+from .sharding import shard_model
 
 __all__ = [
     "get_world_group",
@@ -72,4 +73,5 @@ __all__ = [
     "init_vae_group",
     "init_dit_group",
     "get_dit_group",
+    "shard_model",
 ]

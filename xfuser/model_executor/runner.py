da208be2c2921feacb2bc5950f4c744ba042fcc9
Aleksi Vesanto <avesanto@amd.com>
2026-01-13 08:27:42 -0600

    Refactor runner, ofload responsibilities to model class

--- xfuser/model_executor/runner.py
+++ xfuser/model_executor/runner.py
@@ -1,21 +1,25 @@
 import abc
+import math
 import argparse
 import logging
+import functools
 import torch
 import json
 import copy
 import os
-from typing import Optional, Tuple, Any
+import gc
+import numpy as np
+from typing import Optional, Tuple, Any, Union, Dict
 from torch.profiler import profile, record_function, ProfilerActivity
+from diffusers.utils import load_image
 
 from xfuser import xFuserArgs
 from xfuser.config import FlexibleArgumentParser
 from xfuser.core.distributed import (
     get_world_group,
-    get_data_parallel_world_size,
-    get_data_parallel_rank,
     get_runtime_state,
-    is_dp_last_group,
+    get_sequence_parallel_rank,
+    get_sequence_parallel_world_size,
     initialize_runtime_state,
 )
 
@@ -61,42 +65,28 @@ def register_model(name: str):
 class xFuserModelRunner:
     """ A generic model runner for models supported by xDiT """
 
-    def __init__(self, config: argparse.Namespace):
+    def __init__(self, config: dict):
         self.config = config
-        self.model: Optional[xFuserModel] = None
+        self.model = self._select_model(config.model, config)
         self.is_initialized = False
 
-    def _select_model(self, model_name: str, config: argparse.Namespace) -> "xFuserModel":
+    def _select_model(self, model_name: str, config: dict) -> "xFuserModel":
         """ Select and instantiate model from registry"""
         model = MODEL_REGISTRY.get(model_name, None)
         if not model:
             raise ValueError(f"Model {model_name} not found in registry.")
         return model(config)
 
-    def initialize(self) -> None:
+
+    def initialize(self, input_args: dict) -> None:
         """ Initialize the model and runtime state """
         if self.is_initialized:
             log("Model already initialized, skipping...")
             return
-        engine_args = xFuserArgs.from_cli_args(self.config)
-        engine_config, input_config = engine_args.create_config()
 
         log(f"Initializing model: {self.config.model}")
-        self.model = self._select_model(self.config.model, self.config)
-        self.model.initialize()
+        self.model.initialize(input_args)
 
-        log("Initializing runtime state...")
-        initialize_runtime_state(self.model.pipe, engine_config)
-
-        if self.config.use_torch_compile:
-            log("Torch.compile enabled. Warming up torch compiler with one inference step...")
-            torch._inductor.config.reorder_for_compute_comm_overlap = True
-            self.model.pipe = torch.compile(self.model.pipe, mode="default") # TODO: Configurable
-
-            # one step to warmup the torch compiler
-            preprocessed_args = self._preprocess_args(self.config)
-            preprocessed_args["num_inference_steps"] = 1  # Reduce steps for warmup # TODO: make this more generic
-            self.model.run_timed_pipe(preprocessed_args)
         self.is_initialized = True
         log("Model initialization complete.")
 
@@ -105,47 +95,22 @@ class xFuserModelRunner:
         if not self.is_initialized:
             raise RuntimeError("ModelRunner not initialized. Call initialize() before run().")
 
-        timings = []
-        preprocessed_args = self._preprocess_args(input_args)
+        log("Running model...")
+        output, timings = self.model.run(input_args)
+        return output, timings
 
-        if self.config.warmup_calls:
-            log(f"Warming up model with {self.config.warmup_calls} calls...")
-            for iteration in range(self.config.warmup_calls):
-                log(f"Warmup iteration {iteration + 1}/{self.config.warmup_calls}")
-                self.model.run_timed_pipe(preprocessed_args)
-            log(f"Warmup complete.")
+    def preprocess_args(self, input_args: dict) -> dict:
+        """ Preprocess input arguments before passing them to the model """
+        return self.model.preprocess_args(input_args)
 
-        for iteration in range(self.config.num_iterations):
-            log(f"Running iteration {iteration + 1}/{self.config.num_iterations}")
-            out, timing = self.model.run_timed_pipe(preprocessed_args)
-            timings.append(timing)
-            log(f"Iteration {iteration + 1} completed in {timing:.2f}s")
-
-        log(f"Average time over {self.config.num_iterations} runs: {sum(timings) / len(timings):.2f}s")
-        return out, timings
+    def validate_args(self, input_args: dict):
+        """ Validate input arguments """
+        return self.model.validate_args(input_args)
 
     def profile(self, input_args: dict):
         """ Profile the model execution """
-        schedule = torch.profiler.schedule(
-            wait=self.config.profile_wait,
-            warmup=self.config.profile_warmup,
-            active=self.config.profile_active,
-        )
-        num_repetitions = self.config.profile_wait + self.config.profile_warmup + self.config.profile_active
-        preprocessed_args = self._preprocess_args(input_args)
-        with profile(
-            activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA],
-            schedule=schedule,
-            record_shapes=True, #TODO: make configurable
-            with_stack=False, #TODO: experimental config necessary?
-        ) as profile_object:
-            for iteration in range(num_repetitions):
-                log(f"Profiling iteration {iteration + 1}/{num_repetitions}")
-                with record_function("model_inference"):
-                    out, timing = self.model.run_timed_pipe(preprocessed_args)
-                profile_object.step()
-                log(f"Profiling iteration {iteration + 1} completed in {timing:.2f}s")
-        return out, [], profile_object
+        output, timings, profile_object = self.model.profile(input_args)
+        return output, timings, profile_object
 
     def print_args(self, args: argparse.Namespace):
         """ Print the arguments from the Namespace """
@@ -156,47 +121,23 @@ class xFuserModelRunner:
     def cleanup(self):
         """ Cleanup resources after model execution """
         get_runtime_state().destroy_distributed_env()
+        del self.model.pipe
+        gc.collect()
+        torch.cuda.empty_cache()
+        log("Cleaned up resources.")
+
 
     def save(self, output=None, timings=None, profile=None, save_once: bool = True):
         """ Save model output, timings and profiles to file, if applicable """
         if save_once: # TODO: add rank info to file names so this can even make sense
             if not is_last_process():
                 return
-
         if output:
-            self._save_output(output) # Handle different output types
+            self.model.save_output(output) # Handle different output types
         if timings:
-            timing_file = open(f"{self.config.output_directory}/timings.json", "w")
-            json.dump(timings, timing_file)
-            timing_file.close()
-            log(f"Timings saved to {self.config.output_directory}/timings.json")
+            self.model.save_timings(timings)
         if profile:
-            profile_file = f"{self.config.output_directory}/profile_trace_rank_{get_world_group().rank}.json"
-            profile.export_chrome_trace(profile_file)
-            log(f"Profile trace saved to {profile_file}")
-
-    def _preprocess_args(self, input_args: dict):
-        """ Preprocess input arguments before passing them to the model """
-
-        # Returning the values as dict instead of Namespace for easier handling
-        return copy.deepcopy(vars(input_args))
-
-    def _save_output(self, output):
-        """ Saves the output based on its type """
-        if self.model.model_output_type == "image":
-            from PIL import Image
-            output_image = output.images[0] # TODO: BS > 1
-            output_name = self.model.get_output_name()
-            output_path = f"{self.config.output_directory}/{output_name}.png"
-            output_image.save(output_path)
-            log(f"Output image saved to {output_path}")
-
-        elif self.model.model_output_type == "video":
-            output_name = self.model.get_output_name()
-            output_path = f"{self.config.output_directory}/{output_name}.mp4"
-            output.save(output_path)
-            log(f"Output video saved to {output_path}")
-
+            self.model.save_profile(profile)
 
 class xFuserModel(abc.ABC):
     """ Base class for xFuser models """
@@ -205,11 +146,137 @@ class xFuserModel(abc.ABC):
         self.config = config
         self.pipe = None
 
-    def initialize(self):
+    def initialize(self, input_args):
         """ Load the model pipeline """
+        # These initialize some internal states as a side-effect ...
+        engine_args = xFuserArgs.from_cli_args(self.config)
+        engine_config, input_config = engine_args.create_config()
+
         self.pipe = self._load_model()
 
-    def run_timed_pipe(self, input_args: dict):
+
+        log("Initializing runtime state...")
+        initialize_runtime_state(self.pipe, engine_config)
+
+        if self.config.use_torch_compile:
+            log("Torch.compile enabled. Warming up torch compiler ...")
+            self._compile_model(input_args)
+
+    def _compile_model(self, input_args):
+        """ Compile the model using torch.compile """
+        torch._inductor.config.reorder_for_compute_comm_overlap = True
+        self.pipe = torch.compile(self.pipe, mode="default") # TODO: Configurable
+
+        # two steps to warmup the torch compiler
+        compile_args = copy.deepcopy(input_args)
+        compile_args["num_inference_steps"] = 2  # Reduce steps for warmup # TODO: make this more generic
+        self._run_timed_pipe(compile_args)
+
+
+    def run(self, input_args):
+        """ Run the model with given input arguments and return output and timings """
+        timings = []
+
+        self._run_warmup_calls(input_args)
+        for iteration in range(self.config.num_iterations):
+            log(f"Running iteration {iteration + 1}/{self.config.num_iterations}")
+            out, timing = self._run_timed_pipe(input_args)
+            timings.append(timing)
+            log(f"Iteration {iteration + 1} completed in {timing:.2f}s")
+
+        log(f"Average time over {self.config.num_iterations} runs: {sum(timings) / len(timings):.2f}s")
+        return out, timings
+
+    def _run_warmup_calls(self, input_args):
+        """ Run initial warmup calls if specified """
+        if self.config.warmup_calls:
+            log(f"Warming up model with {self.config.warmup_calls} calls...")
+            for iteration in range(self.config.warmup_calls):
+                log(f"Warmup iteration {iteration + 1}/{self.config.warmup_calls}")
+                self._run_timed_pipe(input_args)
+            log(f"Warmup complete.")
+
+    def profile(self, input_args: dict):
+        """ Profile the model execution """
+        schedule = torch.profiler.schedule(
+            wait=self.config.profile_wait,
+            warmup=self.config.profile_warmup,
+            active=self.config.profile_active,
+        )
+        num_repetitions = self.config.profile_wait + self.config.profile_warmup + self.config.profile_active
+
+        with profile(
+            activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA],
+            schedule=schedule,
+            record_shapes=True, #TODO: make configurable
+            with_stack=False, #TODO: experimental config necessary?
+        ) as profile_object:
+            for iteration in range(num_repetitions):
+                log(f"Profiling iteration {iteration + 1}/{num_repetitions}")
+                with record_function("model_inference"):
+                    out, timing = self._run_timed_pipe(input_args)
+                profile_object.step()
+                log(f"Profiling iteration {iteration + 1} completed in {timing:.2f}s")
+        return out, [], profile_object
+
+    def preprocess_args(self, input_args: dict) -> dict:
+        """ Preprocess input arguments before passing them to the model """
+        if type(input_args) == argparse.Namespace:
+            input_args = vars(input_args)
+        args = copy.deepcopy(input_args)
+        args = self._preprocess_args_images(args)
+        args = self._preprocess_args_custom(args)
+        return args
+
+
+    def _preprocess_args_images(self, input_args: dict) -> dict:
+        """ Preprocess image inputs if necessary """
+        # For legacy reasons, we support several ways to pass images
+        images = []
+        image_path_keys = ["image_path", "image_paths", "input_images"]
+        for key in image_path_keys:
+            if key in input_args:
+                paths = input_args[key]
+                if isinstance(paths, str):
+                    paths = [paths]
+                for path in paths:
+                    image = load_image(path)
+                    images.append(image)
+        input_args["input_images"] = images
+        return input_args
+
+    def _preprocess_args_custom(self, input_args: dict) -> dict:
+        """ Preprocess custom inputs if necessary """
+        return input_args
+
+    def save_output(self, output):
+        """ Saves the output based on its type """
+        if self.model_output_type == "image":
+            from PIL import Image
+            output_image = output.images[0] # TODO: BS > 1
+            output_name = self.get_output_name()
+            output_path = f"{self.config.output_directory}/{output_name}.png"
+            output_image.save(output_path)
+            log(f"Output image saved to {output_path}")
+
+        elif self.model_output_type == "video":
+            output_name = self.get_output_name()
+            output_path = f"{self.config.output_directory}/{output_name}.mp4"
+            output.save(output_path)
+            log(f"Output video saved to {output_path}")
+
+    def save_timings(self, timings: list):
+        timing_file = open(f"{self.config.output_directory}/timings.json", "w")
+        json.dump(timings, timing_file)
+        timing_file.close()
+        log(f"Timings saved to {self.config.output_directory}/timings.json")
+
+    def save_profile(self, profile):
+        profile_file = f"{self.config.output_directory}/profile_trace_rank_{get_world_group().rank}.json"
+        profile.export_chrome_trace(profile_file)
+        log(f"Profile trace saved to {profile_file}")
+
+    def _run_timed_pipe(self, input_args: dict):
         """ Run a a full pipeline with timing information """
 
         events = {
@@ -326,12 +393,13 @@ if __name__ == "__main__":
     args = xFuserArgs.add_cli_args(parser).parse_args()
     runner = xFuserModelRunner(args)
     runner.print_args(args)
-    runner.initialize()
+    input_args = runner.preprocess_args(args)
+    runner.initialize(input_args)
     if args.profile:
-        out, timing, profile = runner.profile(args)
+        out, timing, profile = runner.profile(input_args)
         runner.save(profile=profile)
     else:
-        output, timings = runner.run(args)
+        output, timings = runner.run(input_args)
         runner.save(output=output, timings=timings)
 
     runner.cleanup()

562fe97545f6ec71bf5da7d61bd7c3056e06e143
Kristoffer Torp <kristoffer.torp@amd.com>
2026-05-20 08:04:33 -0500

    Set compile mode to reduce-overhead to lessen CPU overhead

--- xfuser/model_executor/models/runner_models/ltx.py
+++ xfuser/model_executor/models/runner_models/ltx.py
@@ -173,8 +173,7 @@ class xFuserLTX23VideoModel(xFuserModel):
 
     def _compile_model(self, input_args: dict) -> None:
         torch._inductor.config.reorder_for_compute_comm_overlap = True
-        self.pipe.transformer.compile_repeated_blocks(mode="default")
-        self.second_pipe.transformer.compile_repeated_blocks(mode="default")
+        self.pipe.transformer.compile_repeated_blocks(mode="reduce-overhead")
 
         # two steps to warmup the torch compiler
         compile_args = copy.deepcopy(input_args)
@@ -292,8 +291,7 @@ class xFuserLTX2VideoModel(xFuserModel):
 
     def _compile_model(self, input_args: dict) -> None:
         torch._inductor.config.reorder_for_compute_comm_overlap = True
-        self.pipe.transformer.compile_repeated_blocks(mode="default")
-        self.second_pipe.transformer.compile_repeated_blocks(mode="default")
+        self.pipe.transformer.compile_repeated_blocks(mode="reduce-overhead")
 
         # two steps to warmup the torch compiler
         compile_args = copy.deepcopy(input_args)

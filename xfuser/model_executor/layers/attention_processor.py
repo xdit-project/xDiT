2d010938a1445de5cc3e61a3fff5cf846596965d
Qin-sx <Qin_sx@outlook.com>
2026-07-10 00:30:43 +0800

    update xfuser/model_executor/layers/attention_processor.py
    	modified:   xfuser/model_executor/layers/attention_processor.py

--- xfuser/model_executor/layers/attention_processor.py
+++ xfuser/model_executor/layers/attention_processor.py
@@ -40,10 +40,10 @@ from xfuser.core.distributed.runtime_state import get_runtime_state
 from xfuser.model_executor.layers import xFuserLayerBaseWrapper
 from xfuser.model_executor.layers import xFuserLayerWrappersRegister
 from xfuser.logger import init_logger
-from xfuser.envs import PACKAGES_CHECKER
+from xfuser.envs import PACKAGES_CHECKER, TORCH_VERSION
 from packaging import version
 
-if version.parse(torch.__version__).release >= version.parse("2.5.0").release:
+if TORCH_VERSION >= version.parse("2.5.0"):
     from xfuser.model_executor.layers.usp import USP
 else:
     from xfuser.model_executor.layers.usp_legacy import USP
